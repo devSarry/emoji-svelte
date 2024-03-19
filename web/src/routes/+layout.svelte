67bb539080a85999202b3d4a7b1b666dd72f2185
Jonathan Sarry <jonathan.sarry@nortal.com>
2024-03-19 23:16:27 +0200

    Create popover

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -4,12 +4,14 @@
 </script>
 
 <AppShell>
-	<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
-		(title)
-		<svelte:fragment slot="trail">(actions)</svelte:fragment>
-	</AppBar>
+	<svelt:fragment slot="header">
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			(title)
+			<svelte:fragment slot="trail">(actions)</svelte:fragment>
+		</AppBar>
+	</svelt:fragment>
 
 	<slot />
-
+	
 </AppShell>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,13 +1,35 @@
 <script>
-    import { RangeSlider } from '@skeletonlabs/skeleton'
-    
-    let value = 15;
+  import * as Popover from "$lib/components/ui/popover";
+
 
 </script>
 
-<div class="p-4">
-    <div class="grid grid-cols-1 gap-10">
-        <div class="emoji-arena h-96 max-w-[45rem] w-full place-self-center bg-slate-50"></div>
-        <div class="action-bar flex h-10 place-self-center bg-slate-600 w-96"></div>
+<div class="relative max-w-3xl px-4 sm:px-6 lg:px-8 flex flex-col justify-center sm:items-center mx-auto size-full">
+    <div class="emoji-arena h-96 w-full bg-slate-50"></div>
+    <div class="action-bar flex h-10 justify-between bg-slate-600">
+        
+        <button type="button" class="btn bg-gradient-to-br variant-gradient-secondary-tertiary">
+            <span>(icon)</span>
+            <span>Button</span>
+        </button>
+
+        <button type="button" class="btn bg-gradient-to-br variant-gradient-secondary-tertiary">
+            <span>(icon)</span>
+            <span>Button</span>
+        </button>
+
+        <Popover.Root>
+            <Popover.Trigger>
+              <button class="btn">Trigger</button>
+            </Popover.Trigger>
+            <Popover.Content class="w-80">
+              <div class="p-4 bg-slate-600">
+                <h3 class="text-lg font-semibold">Popover Title</h3>
+                <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </div>
+              </Popover.Content>
+        </Popover.Root>
+
     </div>
 </div>
+
